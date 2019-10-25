66011bbeb03805f0d337357dc6c09f57cf56361c
AverianovAndrey <averianov.aa@phystech.edu>
2019-10-25 14:53:19 +0300

    Lab4

--- lab3/игра.py
+++ lab3/игра.py
@@ -8,6 +8,7 @@ canv = Canvas(root,bg='white')
 canv.pack(fill=BOTH,expand=1)
 A=[]
 C=[]
+name=input("your name: ")
 
 t=2000
 rt=0
@@ -118,3 +119,52 @@ new_ball()
 new_rect()
 move_balls()
 mainloop()
+print(rt)
+tx=open('reit.txt','r')
+A=[]
+S=[]
+SR=[]
+for line in tx:
+    nb=0
+    s=""
+    num=""
+    while s!=" ":
+        s=line[nb]
+        if s!=" ":
+            num=num+s
+            nb=nb+1
+    A.append(int(num))
+    S.append(line)
+print(A)
+ttt=0
+B=[]
+j=0
+for i in A:
+    if rt<i:
+        B.append(i)
+        SR.append(S[j])
+        j=j+1
+    else:
+        if ttt==0:
+            B.append(rt)
+            SR.append(str(rt)+" "+name+"\n")
+            ttt=1
+            B.append(i)
+            SR.append(S[j])
+            j=j+1
+        else:
+            B.append(i)
+            SR.append(S[j])
+            j=j+1
+if ttt==0:
+    B.append(rt)
+    SR.append(str(rt)+" "+name+"\n")
+print(B)
+print(S)
+print(SR)
+tx.close()
+tx=open('reit.txt','w')
+for i in SR:
+    tx.write(i)
+
+                                                                                                                                             
